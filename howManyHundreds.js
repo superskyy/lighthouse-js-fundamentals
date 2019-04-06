a987bcb3dc4114a5d8930365f72bcbba3147e49f
superskyy <psynevskyy@gmail.com>
2019-04-06 02:41:44 +0000

    New files added

---
+++ howManyHundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(num) {
+  var result = num / 100;
+  return Math.trunc(result);
+}
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
