7228b4d7b42b18d0243eb95b60828254a42bc6c4
superskyy <psynevskyy@gmail.com>
2019-04-15 01:22:43 +0000

    Sum the Largest Numbers

--- sumLargestNumbers.js
+++ sumLargestNumbers.js
@@ -3,16 +3,17 @@
 // return the sum of the two largest numbers 
 // in that array.
 
-let sumLargestNumbers = function(data) {
-  var largest = [];
-  return Math.max(...data)
-  // for (var i = 0; i < data.length; i++) {
-  // 	data[i]
-  // }
-};
-
-
+function sumLargestNumbers(data) {
+  data.sort(function (a, b) {
+    return a - b;
+  });
+  var array = [];
+  for (var i = data.length - 2; i <= data.length - 1; i++) {
+    array.push(data[i]);
+  } 
+  return array[0] + array[1];
+}
 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]))
