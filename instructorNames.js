27d54f67f8b7bd829fc97d878ec720a3c30cb84f
superskyy <psynevskyy@gmail.com>
2019-04-16 15:21:08 +0000

    Kata 4

---
+++ instructorNames.js
@@ -0,0 +1,15 @@
+const instructorWithLongestName = function(instructors) {
+  // Put your solution here
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]);
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]);
