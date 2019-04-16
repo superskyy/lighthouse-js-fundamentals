c11f948e4bcbb70893256f4ba612cebf8eb79869
superskyy <psynevskyy@gmail.com>
2019-04-16 23:57:02 +0000

    functions

--- instructorNames.js
+++ instructorNames.js
@@ -1,5 +1,9 @@
 const instructorWithLongestName = function(instructors) {
   // Put your solution here
+  var array = {};
+  for (var i = 0; i > instructors.length; i++) {
+  	
+  }
 };
 
 console.log(instructorWithLongestName([
@@ -7,9 +11,9 @@ console.log(instructorWithLongestName([
   {name: "Jeremiah", course: "Web"},
   {name: "Ophilia", course: "Web"},
   {name: "Donald", course: "Web"}
-]);
+]));
 console.log(instructorWithLongestName([
   {name: "Matthew", course: "Web"},
   {name: "David", course: "iOS"},
   {name: "Domascus", course: "Web"}
-]);
+]));
