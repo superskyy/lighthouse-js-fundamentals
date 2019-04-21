ecdf655e458c9e97d4c9db665697f5b0a2f8ba85
superskyy <psynevskyy@gmail.com>
2019-04-21 01:33:06 +0000

    modified

--- percentEncodedString.js
+++ percentEncodedString.js
@@ -1,5 +1,20 @@
 const urlEncode = function(text) {
   // Put your solution here
+  var string = "";
+  // var space = " ";
+  var char = "%20";
+  for (var i = 0; i < text.length; i++) {
+    if (text[0] === " ") {
+      
+      }
+    if (text[i] === " ") {
+      string = string + char;
+    } else {
+      string = string + text[i];
+    }
+    
+  }
+  return string;
 };
 
 console.log(urlEncode("Lighthouse Labs"));
